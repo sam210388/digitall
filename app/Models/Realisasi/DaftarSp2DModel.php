4b36e5513c37206c85ce669ea8b5a2c78dcae9b4
sam210388 <nugraha.sam@gmail.com>
2023-01-30 07:42:24 +0700

    Penambahan SP2D Controller

---
+++ app/Models/Realisasi/DaftarSp2DModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Realisasi;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DaftarSp2DModel extends Model
+{
+    use HasFactory;
+}
